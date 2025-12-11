7e0fdafe65d973e4cd909a39550e81cbf501d0ae
Yug34 <yug.gjr@gmail.com>
2025-12-11 10:09:42 +0530

    kinde auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Button } from "@/components/ui/button"
-import './App.css'
+import {useKindeAuth} from "@kinde-oss/kinde-auth-react";
+import { Navbar01 } from '@/components/ui/shadcn-io/navbar-01';
 
 function App() {
+  const { login, register, isAuthenticated, logout } = useKindeAuth();
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div className="flex flex-col items-center justify-between w-screen h-screen">
+      <Navbar01 />
+      <Button onClick={() => console.log(import.meta.env.VITE_KINDE_CLIENT_ID)} type="button">Click me</Button>
+      <Button onClick={() => register()} type="button">Register</Button>
+      <Button onClick={() => login()} type="button">Log In</Button>
+      {isAuthenticated ? <Button onClick={() => logout()} type="button">Log Out</Button> : null}
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KindeProvider
+    		clientId={import.meta.env.VITE_KINDE_CLIENT_ID!}
+		    domain={import.meta.env.VITE_KINDE_DOMAIN!}
+		    redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI!}
+		    logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI!}
+    >
+      <App />
+    </KindeProvider>
   </StrictMode>,
 )
